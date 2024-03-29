bda275fbdf1aa89959cdd787c7d2e52aaa815035
sanika <sanikaalmeida@gamil.com>
2024-03-29 14:30:16 +0530

    update user route

--- routes/user.js
+++ routes/user.js
@@ -4,7 +4,7 @@ const authenticatetoken=require('../middleware/authenticate')
 const express = require('express')
 const router = express.Router()
 
-router.post('/signup',signup)
-router.post('/login',log)
+router.post('/usersignup',signup)
+router.post('/userlogin',log)
 
 module.exports=router
